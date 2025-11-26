22de81e90d07e53de07d5fd1d0424cfa55939ccd
Monica Mateescu <monica.antonie@gmail.com>
2025-11-26 17:47:56 +0100

    add links to footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router";
+
 const Footer = () => {
   return (
-    <footer className="bg-slate-400  p-4">
-      <div className="container mx-auto  text-slate-100 text-xs"> Footer</div>
+    <footer className="bg-slate-400 p-4">
+      <div className="container mx-auto  text-slate-100 text-xs">
+        <ul className="flex space-x-4 text-xs justify-center">
+          <li>
+            <Link to="/destinations" className="hover:text-slate-500">
+              Destinations
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:text-slate-500">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="hover:text-slate-500">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
