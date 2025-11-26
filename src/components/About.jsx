a244758bd7879757809407317a96db5c27b7f7f4
Monica Mateescu <monica.antonie@gmail.com>
2025-11-26 17:48:27 +0100

    create about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,15 @@
 const About = () => {
-  return <div>Information about the company</div>;
+  return (
+    <section className="py-4 text-center text-slate-400">
+      <h1 className="text-slate-600 text-xl font-semibold mb-4">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dolorem
+        similique placeat, molestiae ipsa harum deserunt neque odio
+        voluptatibus, nobis eum rerum voluptatum cupiditate quisquam nihil
+        debitis, animi labore reiciendis.
+      </p>
+    </section>
+  );
 };
 
 export default About;
