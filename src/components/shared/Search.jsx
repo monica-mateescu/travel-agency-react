d6d0f22c13c9a5af78aa28bdab9f48c955e562d9
Monica Mateescu <monica.antonie@gmail.com>
2025-11-27 08:46:05 +0100

    use daisyUi classes for inputs

--- src/components/shared/Search.jsx
+++ src/components/shared/Search.jsx
@@ -3,49 +3,41 @@ const Search = () => {
   return (
     <section className="py-4 text-center text-slate-600">
       <h1 className="text-slate-600 text-xl font-semibold mb-4">
-        Find your favourite destination
+        Find your favorite destination
       </h1>
       <form className="flex flex-col w-sm mx-auto">
-        <label htmlFor="origin" aria-label="Origin" className="rounded mb-2">
+        <label htmlFor="origin" aria-label="Origin" className="mb-2">
           <input
             type="text"
             name="origin"
             placeholder="Origin"
             id="origin"
-            className="w-full border border-slate-200 p-2 rounded text-xs outline-0"
+            className="w-full input input-sm"
           />
         </label>
-        <label
-          htmlFor="destination"
-          aria-label="Destination"
-          className="rounded mb-2"
-        >
+        <label htmlFor="destination" aria-label="Destination" className="mb-2">
           <input
             type="text"
             name="destination"
             placeholder="Destination"
             id="destination"
-            className="w-full border border-slate-200 p-2 rounded text-xs outline-0"
+            className="w-full input input-sm"
           />
         </label>
-        <label
-          htmlFor="departure"
-          aria-label="Departure"
-          className="rounded mb-2"
-        >
+        <label htmlFor="departure" aria-label="Departure" className="mb-2">
           <input
             type="date"
             name="departure"
             id="departure"
-            className="w-full border border-slate-200 p-2 rounded text-xs outline-0"
+            className="w-full input input-sm"
           />
         </label>
-        <label htmlFor="return" aria-label="Return" className="rounded mb-2">
+        <label htmlFor="return" aria-label="Return" className="mb-2">
           <input
             type="date"
             name="return"
             id="return"
-            className="w-full border border-slate-200 p-2 rounded text-xs outline-0"
+            className="w-full input input-sm"
           />
         </label>
         <Button name={"Search"} />
